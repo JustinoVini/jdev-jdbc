a90c958f78fd182c959d24ea154858d7a5524d27
Vinicius Justino <visilva5690@hotmail.com>
2023-08-25 13:24:32 -0300

    inserindo dependencia do postgresql

--- src/main/java/br/com/jdev/javabd/App.java
+++ src/main/java/br/com/jdev/javabd/App.java
@@ -4,10 +4,8 @@ package br.com.jdev.javabd;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
